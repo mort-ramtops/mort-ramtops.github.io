136b4317f80114b2118e4e8a5c608db86d716688
mort-ramtops <mort-ramtops@users.noreply.github.com>
2021-01-29 10:29:23 +0000

    Deploying to gh-pages from  @ cc30c72e867eff2863edfac7a591460245443e82 🚀

---
+++ sample-data/happend - Copy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#script to download holey hadiths from sunnah.com
+
+address=$1
+#echo $address
+hadith=$(qsel $address ".hadith_narrated, .text_details")
+#echo $hadith
+hadithescaped=$(echo $hadith | sed 's/"//g')
+reference=$(qsel $address "table.hadith_reference > tr" -T "\"ref{{index|incr}}\": \"{{.|text}}\"," --limit 3)
+#reference=$(echo $reference | sed '$s/,$//')
+#echo $reference
+keywords="\"keywords\":[\"dummy\", \"dummy\", \"dummy\"]"
+
+compilation="{\"type\":\"hadith\", \"url\":\"$address\", \"text\":\"$hadithescaped\", $reference $keywords}"
+#echo $compilation
+
+echo $compilation | jq '.'
+
+final=$(cat test.json | jq ".+= [$compilation]")
+
+echo $final | jq '.' > test.json
